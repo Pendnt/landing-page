aa2734e8180496a1d88aa0b7f1a8cb712bce29db
Kade Angell <kade@angell.fyi>
2025-04-21 19:08:07 -0600

    updated footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
           </div>
         </div>
         <div className="mt-8 text-center text-gray-600 text-sm">
-          <p>Turn conversations into leads.</p>
+          <p>Turn conversations into customers.</p>
           <p className="mt-2">© {new Date().getFullYear()} Pendnt. All rights reserved.</p>
         </div>
       </footer>
